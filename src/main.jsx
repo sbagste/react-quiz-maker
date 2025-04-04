94e7d53a4fd25c944bcf8310332cbb88f04b7711
sbagste <s-auguste@gmx.com>
2025-04-04 16:42:30 +0200

    ➕ add react router.

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { HashRouter, Routes, Route } from 'react-router';
 import './index.css';
 import Quiz from '@views/quiz/Quiz.jsx';
 import './style.scss';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Quiz />
+    <HashRouter>
+      <Routes>
+        <Route path='/' element={<Quiz />} />
+      </Routes>
+    </HashRouter>
   </StrictMode>
 );
